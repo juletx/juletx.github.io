5a40b0b6fa4daadca820ae72515851c83676945a
juletx <juletxara@gmail.com>
2026-01-31 12:23:31 +0000

    Deploying to gh-pages from @ juletx/al-folio@869112f29bd8f22f4d64de70844f9df63a63c9f3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "juletx/jekyll-academic-website",
+    "data-repo": "juletx/al-folio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
